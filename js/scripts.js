2ef9dbc01118ec0e8ff6d1983a275e86d49e6d45
Emily Watkins and Julian Alexander <emily@>
2017-11-27 20:44:47 -0800

    Added some html into the results div for the JS to add a pop feature

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,6 @@ function Ticket(movie, time, age) {
 };
 
 Ticket.prototype.calculator = function(ticketPrice) {
-  debugger;
   if (this.movie === "bladerunner") {
     ticketPrice += 3;
   } if (this.time === "matinee"){
@@ -32,7 +31,11 @@ $(document).ready(function() {
 
     var adjustedPrice = new Ticket(inputtedMovie, inputtedTime, inputtedAge);
 
-    $(".result").append(adjustedPrice.calculator(ticketPrice));
+    $(".result").append("<div id='resultDetails'>" +
+                        "<h3>" + adjustedPrice.movie + "</h3>" +
+                        "<h4>Your Showing: " + adjustedPrice.time + "</h4>" +
+                        "<h5> Your ticket will cost $" + adjustedPrice.calculator(ticketPrice) + "</h5>"
+                        + "</div>");
 
 
   });
